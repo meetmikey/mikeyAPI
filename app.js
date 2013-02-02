d57730af34902be9d0750b37c777805ba9af8385
Cody Veal <cveal05@gmail.com>
2013-02-01 18:15:59 -0800

    adding ssl, fixing OAuth, hooking up mongo from common

--- app.js
+++ app.js
@@ -1,26 +1,61 @@
+var commonPath = '../serverCommon/';
+
 var express        = require('express'),
     mongoose       = require('mongoose'),
     passport       = require('passport'),
-    GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+    GoogleStrategy = require('passport-google-oauth').OAuth2Strategy,
+    conf           = require('./conf'),
+    commonConf     = require(commonPath + 'conf'),
+    fs             = require('fs'),
+    https          = require('https');
 
 passport.use(new GoogleStrategy({
-    clientID: 'ABC',
-    clientSecret: 'XYZ',
-    callbackURL: "http:/127.0.0.1/auth/google/callback"
+    clientID: '1020629660865.apps.googleusercontent.com',
+    clientSecret: 'pFvM2J42oBnUFD9sI1ZwITFE',
+    callbackURL: "https://local.meetmikey.com/oauth2callback"
   },
   function(token, tokenSecret, profile, done) {
     // persist!
+    done(null, profile);
   }
 ));
 
-var app = module.exports = express();
-
-app.get('/auth/google', passport.authenticate('google'));
-
-app.get('/auth/google/callback', passport.authenticate('google'), function(req, res) {
-  console.log('authorized!');
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
 });
 
-app.listen(8080, function() {
-  console.log('mikey api now running on port 8080');
+passport.deserializeUser(function(id, done) {
+
+});
+
+var options = {key: fs.readFileSync('keyslocal/privateKey.key'),
+  cert: fs.readFileSync('keyslocal/alpha.magicnotebook.com.crt')};
+
+var app = module.exports = express();
+
+app.configure(function() {
+  app.use(express.logger({ format:'\x1b[1m:method\x1b[0m \x1b[33m:url\x1b[0m :date \x1b[0m :response-time ms' }));
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+
+  var mongoPath = 'mongodb://' + commonConf.mongo.local.host + '/' + commonConf.mongo.local.db;
+  mongoose.connect(mongoPath, function (err) {
+    if (err) throw err;
+  });
+
+  app.use(passport.initialize());
+});
+
+app.get('/auth/google',
+        passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/userinfo.profile',
+                                                  'https://www.googleapis.com/auth/userinfo.email',
+                                                  'https://mail.google.com/mail/feed/atom'] }
+));
+
+app.get('/oauth2callback', passport.authenticate('google', {failureRedirect: '/wtf'}), function(req, res) {
+  console.log('authorized!');
+  res.send('you are authed!');
+});
+
+https.createServer(options, app).listen(8080, function() {
+  console.log('mikey api running on port 8080');
 });
