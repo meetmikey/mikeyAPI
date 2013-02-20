9a36e1054ad60ec9478355be97ede3a984a7bb87
Justin Durack <justin@magicnotebook.com>
2013-02-20 15:46:02 -0800

    Added resolvedURL to link

--- constants.js
+++ constants.js
@@ -23,6 +23,6 @@ define('USE_SPOOFED_USER', false)
 define('SPOOFED_USER_ID', '5119c7b60746d47552000005')
 
 define('DEFAULT_FIELDS_ATTACHMENT', 'filename contentType sentDate sender recipients image hash fileSize')
-define('DEFAULT_FIELDS_LINK', 'url sentDate sender recipients image title text comparableURLHash')
+define('DEFAULT_FIELDS_LINK', 'url resolvedURL sentDate sender recipients image title text comparableURLHash')
 define('URL_EXPIRE_TIME_MINUTES', 30)
 define('SEARCH_THRESHOLD', .01)
