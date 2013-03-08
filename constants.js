11e5a302098659b35c19d4392c42502e947e940b
Sagar Mehta <mehta.sagarv@gmail.com>
2013-03-08 01:28:58 -0800

    fixes to search for images and attachments... given new mapping

--- constants.js
+++ constants.js
@@ -22,8 +22,8 @@ define('SERVER_COMMON', process.env.SERVER_COMMON);
 define('USE_SPOOFED_USER', false);
 define('SPOOFED_USER_ID', '5119c7b60746d47552000005');
 
-define('DEFAULT_FIELDS_ATTACHMENT', 'filename contentType sentDate sender recipients image isImage hash fileSize isDeleted gmMsgId');
-define('DEFAULT_FIELDS_LINK', 'url resolvedURL sentDate sender recipients image title summary comparableURLHash isDeleted gmMsgId');
+define('DEFAULT_FIELDS_ATTACHMENT', 'filename contentType sentDate sender recipients image isImage hash fileSize isDeleted gmMsgId gmMsgHex');
+define('DEFAULT_FIELDS_LINK', 'url resolvedURL sentDate sender recipients image title summary comparableURLHash isDeleted gmMsgId gmMsgHex');
 define('URL_EXPIRE_TIME_MINUTES', 30);
 define('SEARCH_THRESHOLD', .02);
 define('ACTIVE_CONNECTION_REQUEUE_CUTOFF', 60*1000*2) // 2 minutes
