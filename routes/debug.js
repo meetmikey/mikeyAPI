6c1b8a9c5074365751b74b2aff1e975179af655a
Sagar Mehta <mehta.sagarv@gmail.com>
2013-04-05 11:56:37 -0700

    debug route modified

--- routes/debug.js
+++ routes/debug.js
@@ -5,10 +5,10 @@ var winston = require(serverCommon + '/lib/winstonWrapper').winston;
 exports.postClientBug = function(req, res) {
 
   var type = req.body.bugType;
-  var info = req.body.info;
+  var data = req.body.data;
   var userEmail = req.body.userEmail;
 
-  winston.doError ('Bug posted from client', {type : type, info : info, email : userEmail});
+  winston.doError ('Bug posted from client', {type : type, data : data, email : userEmail});
 
   res.send (200);
 }
