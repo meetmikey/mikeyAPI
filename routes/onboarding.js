55180148ffd0f1da9c1a387246de981b87487b9c
Sagar Mehta <mehta.sagarv@gmail.com>
2013-04-19 22:32:40 -0700

    24 hour timestamp to not even have to check mail states to say onboarding "done"

--- routes/onboarding.js
+++ routes/onboarding.js
@@ -10,7 +10,7 @@ exports.getOnboardingState = function (req, res) {
   var userId = req.user._id;
 
   UserOnboardingStateModel.findOne ({userId : userId}, 
-    '_id userId lastCompleted',
+    '_id userId lastCompleted mikeyMailTS',
     function (err, foundState) {
       if (err) {
         winston.doMongoError(err, null, res);
@@ -20,26 +20,30 @@ exports.getOnboardingState = function (req, res) {
           res.send ({'progress' : 0});
         }
         else {
-
-          // check whether 75% of mails with mmDone=true are also mailReaderState = done
-          MailModel.count ({userId : userId, mmDone : true}, function (err, mmDoneCount) {
-            if (err) {
-              winston.doMongoError (err, {'err' : 'mongo error'}, res);
-            } else {
-              MailModel.count ({userId: userId, mmDone : true, mailReaderState : 'done'}, function (err, readerDoneCount) {
-                if (err) {
-                  winston.doMongoError (err, {'err' : 'mongo error'}, res);
-                } else if (readerDoneCount/mmDoneCount > constants.DONE_THRESHOLD) {
-                  res.send ({'progress' : 1});
-                } else {
-                  winston.doInfo ('Progress of onboarding not high enough', {progress : readerDoneCount/mmDoneCount});
-                  res.send ({'progress' : 0});
-                }
-              });
-            }
-          });
+          // check if it's been > 24 hours since onboarding complete
+          if (foundState.mikeyMailTS < new Date(Date.now () - 60*1000*60*24)) {
+            winston.doInfo ('24 hours since onboarding, pretty sure we\'re done');
+            res.send ({'progress' : 1});
+          } else {
+            // check whether 75% of mails with mmDone=true are also mailReaderState = done
+            MailModel.count ({userId : userId, mmDone : true}, function (err, mmDoneCount) {
+              if (err) {
+                winston.doMongoError (err, {'err' : 'mongo error'}, res);
+              } else {
+                MailModel.count ({userId: userId, mmDone : true, mailReaderState : 'done'}, function (err, readerDoneCount) {
+                  if (err) {
+                    winston.doMongoError (err, {'err' : 'mongo error'}, res);
+                  } else if (readerDoneCount/mmDoneCount > constants.DONE_THRESHOLD) {
+                    res.send ({'progress' : 1});
+                  } else {
+                    winston.doInfo ('Progress of onboarding not high enough', {progress : readerDoneCount/mmDoneCount});
+                    res.send ({'progress' : 0});
+                  }
+                });
+              }
+            });
+          }
         }
-
       }
       else {
         res.send ({'progress' : 0});
